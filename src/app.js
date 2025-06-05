38414c4403bed77f3463b7e2e98f3afed1b45840
Sallai András <andrewteki@gmail.com>
2025-06-05 20:22:18 +0200

    Kezdés

---
+++ src/app.js
@@ -0,0 +1,37 @@
+const cars = [
+  {
+    id: 1,
+    plate: 'ABC123',
+    name: 'Ford',
+    model: 'Mustang',
+    year: 2022
+  },
+  {
+    id: 2,
+    plate: 'DEF456',
+    name: 'BMW',
+    model: 'X5',
+    year: 2021
+  },
+  {
+    id: 3,
+    plate: 'GHI789',
+    name: 'Toyota',
+    model: 'Camry',
+    year: 2020
+  },
+  {
+    id: 4,
+    plate: 'JKL012',
+    name: 'Honda',
+    model: 'Civic',
+    year: 2019
+  },
+  {
+    id: 5,
+    plate: 'MNO345',
+    name: 'Nissan',
+    model: 'Altima',
+    year: 2018
+  }
+]
